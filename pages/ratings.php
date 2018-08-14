f3a10429dba8426b509cebcfb2a8ebbb007ca4f3
mbikinin <mbikinin@d-l-s.ru>
2018-08-14 13:14:56 +0300

    Fix colledge

--- pages/ratings.php
+++ pages/ratings.php
@@ -237,6 +237,8 @@ class ratings_Page extends View
         $params->kvota = $kvota;
 
         $response = self::connectWsdl("entrants?wsdl")->getEntrantsList($params);
+        Debug::dump($params);
+
         $extExamScoreArray = array();
         $schoolDiscScoreArray = array();
         if (!empty($response-> return)) {
