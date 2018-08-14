520a6c0152650c1a4d5e108dcc13c489bf078c89
mbikinin <mbikinin@d-l-s.ru>
2018-08-14 14:18:32 +0300

    Fix colledge

--- pages/ratings.php
+++ pages/ratings.php
@@ -264,7 +264,7 @@ class ratings_Page extends View
                     "extExamScores2" => !empty($extExamScoreArray) ? $extExamScoreArray : array(),
                     "schoolDiscScore" => !empty($schoolDiscScoreArray) ? $schoolDiscScoreArray : array(),
                     "schoolDiscScore2" => !empty($schoolDiscScoreArray) ? $schoolDiscScoreArray : array(),
-                    "status" => !empty($res->status) ? $res->status : null
+                    "status" => isset($res->status) ? $res->status : null
                 );
 			}
 			self::$page['content'] = array();
